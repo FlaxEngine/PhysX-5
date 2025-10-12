a2b0b360399fd9495477cf69141260f781eccf62
Ari Vuollet <ari.vuollet@goat.moe>
2025-10-12 16:36:44 +0300

    Add support for building with Visual Studio 2026

--- physx/generate_projects.bat
+++ physx/generate_projects.bat
@@ -39,6 +39,11 @@ for /f "usebackq tokens=*" %%i in (`"%PM_vswhere_PATH%\VsWhere.exe  -version [17
 	set VS170PATH="%%i"
 )
 
+for /f "usebackq tokens=*" %%i in (`"%PM_vswhere_PATH%\VsWhere.exe  -version [18.0,19.0) -latest -property installationPath"`) do (
+	set Install2026Dir=%%i
+	set VS180PATH="%%i"
+)
+
 if exist "%Install2017Dir%\VC\Auxiliary\Build\Microsoft.VCToolsVersion.default.txt" (
   pushd "%Install2017Dir%\VC\Auxiliary\Build\"
   set /p Version=<Microsoft.VCToolsVersion.default.txt
@@ -65,6 +70,18 @@ if exist "%Install2019Dir%\VC\Auxiliary\Build\Microsoft.VCToolsVersion.default.t
 
 if exist "%Install2022Dir%\VC\Auxiliary\Build\Microsoft.VCToolsVersion.default.txt" (
   pushd "%Install2022Dir%\VC\Auxiliary\Build\"
+  set /p Version=<Microsoft.VCToolsVersion.default.txt
+  for /f "delims=" %%x in (Microsoft.VCToolsVersion.default.txt) do (
+	if not %%x=="" (
+	  rem Example hardcodes x64 as the host and target architecture, but you could parse it from arguments
+	  set VS170CLPATH="%Install2022Dir%\VC\Tools\MSVC\%%x\bin\HostX64\x64\cl.exe"
+	)
+  )
+  popd
+)
+
+if exist "%Install2026Dir%\VC\Auxiliary\Build\Microsoft.VCToolsVersion.default.txt" (
+  pushd "%Install2026Dir%\VC\Auxiliary\Build\"
   set /p Version=<Microsoft.VCToolsVersion.default.txt
   for /f "delims=" %%x in (Microsoft.VCToolsVersion.default.txt) do (
 	if not %%x=="" (
